c012f9be67ec8d74372c0215af67fdbed9c4073c
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-26 22:30:51 +0900

    add index on users

--- webapp/mysql/migration/0_index.sql
+++ webapp/mysql/migration/0_index.sql
@@ -1,2 +1,3 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE products ADD FULLTEXT INDEX ft_idx_products_name_description (name, description) WITH PARSER ngram;
+ALTER TABLE users ADD INDEX idx_users_user_name (user_name);
