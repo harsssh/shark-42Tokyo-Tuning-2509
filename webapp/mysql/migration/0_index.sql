58b9f2ffd194b53a25c61cc09637075729dbfb8b
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-28 03:56:20 +0900

    fix index

--- webapp/mysql/migration/0_index.sql
+++ webapp/mysql/migration/0_index.sql
@@ -2,10 +2,10 @@ ALTER TABLE products
     ALGORITHM=INPLACE,
     LOCK=NONE,
     ADD INDEX idx_products_value_product_id (value DESC, product_id),
-    ADD INDEX idx_products_value_product_id (value, product_id),
-    ADD INDEX idx_products_weight_product_id (weight DESC, product_id),
+    ADD INDEX idx_products_value_desc_product_id (value, product_id),
+    ADD INDEX idx_products_weight_desc_product_id (weight DESC, product_id),
     ADD INDEX idx_products_weight_product_id (weight, product_id),
-    ADD INDEX idx_products_name_product_id (name DESC, product_id),
+    ADD INDEX idx_products_name_desc_product_id (name DESC, product_id),
     ADD INDEX idx_products_name_product_id (name, product_id),
     ADD INDEX idx_products_name (name),
     ADD INDEX idx_products_product_id_weight_value (product_id, weight, value);
