6d6f6267f483bf8aed9b96312fed2780824d2c7f
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-27 09:13:03 +0900

    migration 壊してた

--- webapp/mysql/migration/0_index.sql
+++ webapp/mysql/migration/0_index.sql
@@ -1,9 +1,12 @@
 ALTER TABLE products
     -- products の検索用
     ADD FULLTEXT INDEX ft_idx_products_name_description (name, description) WITH PARSER ngram,
-    -- orders の検索用
-    ADD FULLTEXT INDEX ft_idx_name (name) WITH PARSER ngram,
     ADD INDEX idx_name (name);
+
+-- fulltext index は 1 度に 1 つしか作れないので分ける
+ALTER TABLE products
+    -- orders の検索用
+    ADD FULLTEXT INDEX ft_idx_name (name) WITH PARSER ngram;
 
 -- ログインの改善
 ALTER TABLE users ADD INDEX idx_users_user_name (user_name);
