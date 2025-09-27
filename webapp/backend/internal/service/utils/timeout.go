078feb809ff1a9b0a4cf36d638669b517f743a56
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-27 13:44:17 +0900

    shorten timeout

--- webapp/backend/internal/service/utils/timeout.go
+++ webapp/backend/internal/service/utils/timeout.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-var defaultTimeout = 30 * time.Second
+var defaultTimeout = 20 * time.Second
 
 // 終わらない処理などによる無限ループを防ぐため、タイムアウト付きで処理を実行する
 func WithTimeout(parent context.Context, fn func(ctx context.Context) error) error {
