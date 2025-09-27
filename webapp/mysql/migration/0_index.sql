097a876f411a70bbb81aaf2437f5804df41332f3
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-27 09:01:44 +0900

    add index をまとめる

--- webapp/mysql/migration/0_index.sql
+++ webapp/mysql/migration/0_index.sql
@@ -1,11 +1,12 @@
-ALTER TABLE products ADD FULLTEXT INDEX ft_idx_products_name_description (name, description) WITH PARSER ngram;
+ALTER TABLE products
+    -- products の検索用
+    ADD FULLTEXT INDEX ft_idx_products_name_description (name, description) WITH PARSER ngram,
+    -- orders の検索用
+    ADD FULLTEXT INDEX ft_idx_name (name) WITH PARSER ngram,
+    ADD INDEX idx_name (name);
 
 -- ログインの改善
 ALTER TABLE users ADD INDEX idx_users_user_name (user_name);
-
--- orders の検索のときに name 単体の検索をしたい
-ALTER TABLE products ADD FULLTEXT INDEX ft_idx_name (name) WITH PARSER ngram;
-ALTER TABLE products ADD INDEX idx_name (name);
 
 -- delivery plan を作るときの orders の join に使う (covering index)
 ALTER TABLE orders ADD INDEX idx_orders_product_id_shipped_status_order_id (product_id, shipped_status, order_id);
