80fa2632af591ae3d77c2a5edf2a3ff46e72ad1a
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-28 03:53:23 +0900

    add index on orders

--- webapp/mysql/migration/0_index.sql
+++ webapp/mysql/migration/0_index.sql
@@ -1,4 +1,6 @@
 ALTER TABLE products
+    ALGORITHM=INPLACE,
+    LOCK=NONE,
     ADD INDEX idx_products_value_product_id (value DESC, product_id),
     ADD INDEX idx_products_value_product_id (value, product_id),
     ADD INDEX idx_products_weight_product_id (weight DESC, product_id),
@@ -9,7 +11,15 @@ ALTER TABLE products
     ADD INDEX idx_products_product_id_weight_value (product_id, weight, value);
 
 -- ログインの改善
-ALTER TABLE users ADD INDEX idx_users_user_name (user_name);
+ALTER TABLE users
+    ALGORITHM=INPLACE,
+    LOCK=NONE,
+    ADD INDEX idx_users_user_name (user_name);
 
--- delivery plan を作るときの orders の join に使う (covering index)
-ALTER TABLE orders ADD INDEX idx_orders_shipped_status_product_id_order_id (shipped_status, product_id, order_id);
+ALTER TABLE orders
+    ALGORITHM=INPLACE,
+    LOCK=NONE,
+    -- delivery plan を作るときの orders の join に使う (covering index)
+    ADD INDEX idx_orders_shipped_status_product_id_order_id (shipped_status, product_id, order_id),
+    ADD INDEX idx_orders_user_id_shipped_status_order_id (user_id, shipped_status, order_id),
+    ADD INDEX idx_orders_user_id_created_at (user_id, created_at);
