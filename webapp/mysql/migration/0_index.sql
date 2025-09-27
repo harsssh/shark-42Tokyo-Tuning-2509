b6a70d9a3c8b12309c0287790003fccf78c5903f
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-27 12:09:55 +0900

    restore がマシになってほしい

--- webapp/mysql/migration/0_index.sql
+++ webapp/mysql/migration/0_index.sql
@@ -1,15 +1,23 @@
 ALTER TABLE products
+    ALGORITHM=INPLACE,
     -- products の検索用
-    ADD FULLTEXT INDEX ft_idx_products_name_description (name, description) WITH PARSER ngram,
-    ADD INDEX idx_name (name);
+    ADD FULLTEXT INDEX ft_idx_products_name_description (name, description) WITH PARSER ngram;
 
 -- fulltext index は 1 度に 1 つしか作れないので分ける
 ALTER TABLE products
+    ALGORITHM=INPLACE,
     -- orders の検索用
-    ADD FULLTEXT INDEX ft_idx_name (name) WITH PARSER ngram;
+    ADD FULLTEXT INDEX ft_idx_name (name) WITH PARSER ngram,
+    ADD INDEX idx_name (name);
 
 -- ログインの改善
-ALTER TABLE users ADD INDEX idx_users_user_name (user_name);
+ALTER TABLE users
+    ALGORITHM=INPLACE,
+    LOCK=NONE,
+    ADD INDEX idx_users_user_name (user_name);
 
 -- delivery plan を作るときの orders の join に使う (covering index)
-ALTER TABLE orders ADD INDEX idx_orders_product_id_shipped_status_order_id (product_id, shipped_status, order_id);
+ALTER TABLE orders
+    ALGORITHM=INPLACE,
+    LOCK=NONE,
+    ADD INDEX idx_orders_product_id_shipped_status_order_id (product_id, shipped_status, order_id);
