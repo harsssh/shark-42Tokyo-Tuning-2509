b45fdb1a3b96e01bb4434407259320ccc588cad7
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-28 21:29:44 +0900

    products の検索用に index を追加

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,1 +1,8 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+ALTER TABLE products
+    ADD INDEX idx_products_name_product_id (name, product_id),
+    ADD INDEX idx_products_name_desc_product_id (name DESC, product_id),
+    ADD INDEX idx_products_value_product_id (value, product_id),
+    ADD INDEX idx_products_value_desc_product_id (value DESC, product_id),
+    ADD INDEX idx_products_weight_product_id (weight, product_id),
+    ADD INDEX idx_products_weight_desc_product_id (weight DESC, product_id);
